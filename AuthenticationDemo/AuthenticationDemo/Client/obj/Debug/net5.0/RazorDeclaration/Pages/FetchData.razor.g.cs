e4f965e5cd2128b7592a00216b74a037f6149dfb
shujamughal <shujamughal@gmail.com>
2021-03-11 16:29:45 +0500

    role base authorization

--- AuthenticationDemo/AuthenticationDemo/Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ AuthenticationDemo/AuthenticationDemo/Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,105 +13,105 @@ namespace AuthenticationDemo.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 1 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 2 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 3 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 4 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 5 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 6 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 7 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 8 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 9 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 10 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using AuthenticationDemo.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
+#line 11 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\_Imports.razor"
 using AuthenticationDemo.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
+#line 2 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
+#line 3 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
+#line 4 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
 using AuthenticationDemo.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
+#line 5 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
            [Authorize]
 
 #line default
@@ -126,7 +126,7 @@ using AuthenticationDemo.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 41 "C:\Users\Admin\source\repos\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
+#line 41 "C:\Users\Admin\Documents\GitHub\Blazor\AuthenticationDemo\AuthenticationDemo\Client\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
